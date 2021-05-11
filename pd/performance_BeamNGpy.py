4f4cb931f9a0637773ee8303ed11a5bcb80e6c9e
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2021-05-12 00:03:55 +0200

    Small refactoring, directory created for results.

--- pd/performance_BeamNGpy.py
+++ pd/performance_BeamNGpy.py
@@ -5,26 +5,28 @@ Small performance checks.
 
 import cProfile
 import pstats
+import os
 
 # result with 10000, lot of with 100000
 # BIG_NUMBER = 1000000
 BIG_NUMBER = 100000000
 
 """
-Dict lines
 Project: BeamNGpy
 Hash: e1e0f9db91aaa948bb4832e9c5e88f3c86d164f5
 Message: Implement shared memory transmission for sensors and demo scenario
 """
+
+
 def one_line_dict():
     for i in range(1, BIG_NUMBER):
-        data = dict(type='test')
+        _data = dict(type='test')
 
 
 def two_line_dict():
     for i in range(1, BIG_NUMBER):
-        data = dict()
-        data["type"] = "TEST"
+        _data = dict()
+        _data["type"] = "TEST"
 
 
 def dict_lines():
@@ -38,24 +40,24 @@ def dict_lines():
     """
 
     print("dict one line")
-    cProfile.run("one_line_dict()",'profile_results_dol')
-    results("profile_results_dol", "formatted_profile_dol.txt")
-    print_results("formatted_profile_dol.txt")
+    cProfile.run("one_line_dict()", 'results/profile_results_dol')
+    results("results/profile_results_dol", "results/formatted_profile_dol.txt")
+    print_results("results/formatted_profile_dol.txt")
 
     print("dict two line")
-    cProfile.run("two_line_dict()",'profile_results_dtl')
-    results("profile_results_dtl", "formatted_profile_dtl.txt")
-    print_results("formatted_profile_dtl.txt")
+    cProfile.run("two_line_dict()", 'results/profile_results_dtl')
+    results("results/profile_results_dtl", "results/formatted_profile_dtl.txt")
+    print_results("results/formatted_profile_dtl.txt")
 
 
 def dict_not_written():
     for i in range(1, BIG_NUMBER):
-        data = {"type": "TEST"}
+        _data = {"type": "TEST"}
 
 
 def dict_written():
     for i in range(1, BIG_NUMBER):
-        data = dict(type="TEST")
+        _data = dict(type="TEST")
 
 
 def dict_writing_style():
@@ -69,17 +71,17 @@ def dict_writing_style():
     """
 
     print("dict not written")
-    cProfile.run("dict_not_written()",'profile_results_dnw')
-    results("profile_results_dnw", "formatted_profile_dnw.txt")
-    print_results("formatted_profile_dnw.txt")
+    cProfile.run("dict_not_written()", 'results/profile_results_dnw')
+    results("results/profile_results_dnw", "results/formatted_profile_dnw.txt")
+    print_results("results/formatted_profile_dnw.txt")
 
     print("dict written")
-    cProfile.run("dict_written()",'profile_results_dw')
-    results("profile_results_dw", "formatted_profile_dw.txt")
-    print_results("formatted_profile_dw.txt")
+    cProfile.run("dict_written()", 'results/profile_results_dw')
+    results("results/profile_results_dw", "results/formatted_profile_dw.txt")
+    print_results("results/formatted_profile_dw.txt")
 
 
-def results(name_file, name_new_file):
+def results(name_file: str, name_new_file: str):
     file = open(name_new_file, 'w')
     profile = pstats.Stats(name_file, stream=file)
     profile.sort_stats('cumulative')
@@ -87,15 +89,29 @@ def results(name_file, name_new_file):
     file.close()
 
 
-def print_results(formatted_file_name):
-     f = open(formatted_file_name, "r")
-     print(f.read()) 
+def print_results(formatted_file_name: str):
+    f = open(formatted_file_name, "r")
+    print(f.read())
+
+
+def create_results_dir():
+    path = "results"
+    if not os.path.exists(path):
+        try:
+            os.mkdir(path)
+        except OSError:
+            print(f"Failed to create directory at {path}.")
+        else:
+            print(f"Directory {path} created.")
+    else:
+        print(f"Directory already existed at {path}.")
 
 
 def main():
+    create_results_dir()
     print("dict one line (new) VS dict two lines (old)")
     dict_lines()
-    print("dict not writteni(old) VS dict written(new)")
+    print("dict not written(old) VS dict written(new)")
     dict_writing_style()
 
 
