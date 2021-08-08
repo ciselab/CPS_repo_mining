94ef3e7ee884eed7c9c1ec68b9ce3cacc15712d4
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2021-08-08 19:40:42 +0200

    Fix typos and PEP8 compliance.

--- pd/clone_repos.py
+++ pd/clone_repos.py
@@ -1,39 +1,40 @@
 import pd.dict_repo_list
-import sys, os
+import sys
+import os
 from git import Repo
+
 
 def main(argv):
     if len(argv) != 1:
         print_help()
         exit()
-    output_direcotry = argv[0]
+    output_directory = argv[0]
 
-    if not os.path.exists(output_direcotry):
+    if not os.path.exists(output_directory):
         print_help()
         exit()
 
-    output_direcotry_abspath = os.path.abspath(output_direcotry)
+    output_directory_abspath = os.path.abspath(output_directory)
     projects_list = list(pd.dict_repo_list.projects.keys())
     number_of_projects = str(len(projects_list))
-    print("Cloning "+number_of_projects+" projects to "+output_direcotry_abspath)
+    print("Cloning "+number_of_projects+" projects to "+output_directory_abspath)
 
     counter = 1
     for project in projects_list:
         repositories_info = pd.dict_repo_list.projects[project]
         remote_address = repositories_info["remote"]
         print("("+str(counter)+"/"+number_of_projects+") project: "+project+" - Remote: "+remote_address)
-        project_outout_directory = os.path.join(output_direcotry_abspath,project)
+        project_output_directory = os.path.join(output_directory_abspath, project)
 
-        if os.path.exists(project_outout_directory):
-            print("Warning: "+project_outout_directory+" already exists. Removing the directory ...")
-            os.rmdir(project_outout_directory)
+        if os.path.exists(project_output_directory):
+            print("Warning: "+project_output_directory+" already exists. Removing the directory ...")
+            os.rmdir(project_output_directory)
         
-        os.makedirs(project_outout_directory)
+        os.makedirs(project_output_directory)
 
-        Repo.clone_from(remote_address,project_outout_directory)
-        
+        Repo.clone_from(remote_address, project_output_directory)
 
-        counter+=1
+        counter += 1
     
 
 def print_help():
