603ba319d647082f264b03393765270e5473c3fd
Imara van Dinten <80925368+IvDinten@users.noreply.github.com>
2021-09-07 11:37:44 +0200

    Removing files with a read-only bit set is now possible. General refactoring, added docstring, changed prints and using argparse.

--- pd/clone_repos.py
+++ pd/clone_repos.py
@@ -1,49 +1,67 @@
-import pd.dict_repo_list
-import sys
+import pathlib
 import os
+import shutil
+import argparse
+import stat
 from git import Repo
+import pd.dict_repo_list
 
 
-def main(argv):
-    if len(argv) != 1:
-        print_help()
-        exit()
-    output_directory = argv[0]
+def clone_repositories(output_directory: pathlib.Path):
+    """
+    Clone repositories from pd.dict_repo_list to given location.
 
-    if not os.path.exists(output_directory):
-        print_help()
-        exit()
+    Args:
+        output_directory: Location to clone repositories to.
+    """
+    projects_list = pd.dict_repo_list.projects.keys()
+    number_of_projects = len(projects_list)
 
-    output_directory_abspath = os.path.abspath(output_directory)
-    projects_list = list(pd.dict_repo_list.projects.keys())
-    number_of_projects = str(len(projects_list))
-    print("Cloning "+number_of_projects+" projects to "+output_directory_abspath)
-
-    counter = 1
-    for project in projects_list:
+    print(f"Cloning {number_of_projects} projects to {output_directory}")
+    for index, project in enumerate(projects_list, start=1):
         repositories_info = pd.dict_repo_list.projects[project]
         remote_address = repositories_info["remote"]
-        print("("+str(counter)+"/"+number_of_projects+") project: "+project+" - Remote: "+remote_address)
-        project_output_directory = os.path.join(output_directory_abspath, project)
+        print(f"({index}/{number_of_projects}) project: {project} - Remote: {remote_address}")
+        project_output_directory = output_directory.joinpath(project)
 
-        if os.path.exists(project_output_directory):
-            print("Warning: "+project_output_directory+" already exists. Removing the directory ...")
-            os.rmdir(project_output_directory)
-        
+        if project_output_directory.exists():
+            print(f"Warning: {project_output_directory} already exists. Removing the directory and re-cloning...")
+            try:
+                shutil.rmtree(project_output_directory, onerror=remove_readonly)
+            except OSError as e:
+                print(f"Error project: {project_output_directory} : {e.strerror}")
         os.makedirs(project_output_directory)
-
         Repo.clone_from(remote_address, project_output_directory)
 
-        counter += 1
-    
 
-def print_help():
+def remove_readonly(func, path, _):
     """
-    Print help text for wrong user input.
+    Removing a directory containing files caused an issue when the read-only bit has been set on some of the files.
+    Clears the read-only bit, so the removal can be retried.
+    Args:
+        func: The function that triggered the exception.
+        path: The path to the file that triggered the exception.
+        _: Exception information, not used here but `remove_readonly` needs to accept 3 parameters.
     """
-    print("Expected: one input argument indicating a directory for saving the cloned repositories.")
-    print("This directory should already exist.")
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Repository cloning utility.')
+    parser.add_argument('output_directory', type=pathlib.Path, help='Path to save the repositories to.')
+    args = parser.parse_args()
+
+    output_directory = args.output_directory.absolute()
+
+    if not output_directory.exists():
+        try:
+            os.makedirs(output_directory)
+        except OSError as e:
+            print(f"Could not create directory at location: {output_directory}; Error: {e}")
+
+    clone_repositories(output_directory)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
